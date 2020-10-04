80ae83b53b8bda6209094c3f36bd8bb3fc6f9ae3
bradjones8 <56162788+bradjones8@users.noreply.github.com>
2020-10-04 01:58:39 -0400

    Update index.js
    Added the schema for the posts and added the /makepost route to the app instance which will insert an example post to the cloud db.
    The specified route also will print every post's json data to the response.
    New code begins at line 62.

--- models/index.js
+++ models/index.js
@@ -58,8 +58,81 @@ app.get('/api/getList', (req, res) => {
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// An api endpoint that returns a short list of items
 
+const postSchema = new Schema({
+    postId: Number,
+    //id of person needing aid
+    needsAidId: Number,
+    //id of person giving aid
+    aidId: Number,
+    //name of person needing aid
+    needsAidName: {
+        first: String,
+        last: String
+    },
+    //name of person giving aid
+    aidName: {
+        first: String,
+        last: String
+},
+    title: String,
+    postDesc: String,
+    postResponse: String,
+    upVotes: Number,
+    downVotes: Number
+
+});
+const Post = mongoose.model('Post', postSchema);
+
+app.get('/makepost', (req, res) => {
+    postData = {
+        postId: 1,
+        //id of person needing aid
+        needsAidId: 1,
+        //id of person giving aid
+        aidId: null,
+        //name of person needing aid
+        needsAidName: {
+            first: 'Jane',
+            last: 'Doe'
+        },
+        //name of person giving aid
+        aidName: {
+            first: '',
+            last: ''
+        },
+        title: "Walk to Schoolbus",
+        postDesc: "I am in need of a local volunteer who can help walk my son to the bus stop.",
+        postResponse: "",
+        upVotes: null,
+        downVotes: null
+        
+
+    };
+
+    const newPost = Post(postData); //Instance of post model
+    //saves instance of post model
+    newPost.save((error) => {
+        if (error) {
+            console.log('Data did not save');
+        } else {
+            console.log('Post has been saved!');
+        }
+    })
+
+    //prints all post json data to response
+    Post.find({})
+        .then((ourdata) => {
+            console.log('Data: ', ourdata);
+            res.json(ourdata);
+        })
+        .catch((error) => {
+            console.log('error: ', error);
+        });
+    
+
+
+});
 
 
 
